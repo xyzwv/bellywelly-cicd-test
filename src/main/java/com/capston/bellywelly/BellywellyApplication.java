01235c8a3982add2137ff95c12a7b38add38f8a4
yeji <yeji.lee.kim@gmail.com>
2023-12-01 19:42:50 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/capston/bellywelly/BellywellyApplication.java
@@ -0,0 +1,13 @@
+package com.capston.bellywelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellywellyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellywellyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capston/bellywelly/BellywellyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capston.bellywelly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellywellyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
