edf189a4d4fcd186f7956afdea705889f4d2995f
yeji <yeji.lee.kim@gmail.com>
2024-02-19 20:57:58 +0900

    fix: 식단 기록 응답의 식사 시간을 한글로 반환하도록 수정

--- src/main/java/com/capston/bellywelly/domain/record/dto/DietRecordResponseDto.java
+++ src/main/java/com/capston/bellywelly/domain/record/dto/DietRecordResponseDto.java
@@ -3,9 +3,12 @@ package com.capston.bellywelly.domain.record.dto;
 import java.util.List;
 
 import com.capston.bellywelly.domain.record.entity.Diet;
+import com.capston.bellywelly.domain.record.entity.Mealtime;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class DietRecordResponseDto {
 
 	private String image;
@@ -17,7 +20,7 @@ public class DietRecordResponseDto {
 	@Builder
 	public DietRecordResponseDto(Diet diet, List<String> meal, FodmapListDto fodmapList, NutrientDto nutrient) {
 		this.image = diet.getImage();
-		this.mealtime = diet.getMealtime().name();
+		this.mealtime = Mealtime.to(diet.getMealtime());
 		this.meal = meal;
 		this.fodmapList = fodmapList;
 		this.nutrient = nutrient;
