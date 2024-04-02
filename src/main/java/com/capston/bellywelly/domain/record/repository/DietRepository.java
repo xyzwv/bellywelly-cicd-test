af1a473a1386de09371cabaa16ba7294b29fac9f
yeji <yeji.lee.kim@gmail.com>
2024-04-02 17:16:10 +0900

    feat: date와 mealtime으로 식단 1개 조회하는 서비스 메서드 추가

--- src/main/java/com/capston/bellywelly/domain/record/repository/DietRepository.java
+++ src/main/java/com/capston/bellywelly/domain/record/repository/DietRepository.java
@@ -1,8 +1,13 @@
 package com.capston.bellywelly.domain.record.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.capston.bellywelly.domain.record.entity.Diet;
 
 public interface DietRepository extends JpaRepository<Diet, Long> {
+
+	List<Diet> findAllByCreatedDateBetween(LocalDateTime time1, LocalDateTime time2);
 }
