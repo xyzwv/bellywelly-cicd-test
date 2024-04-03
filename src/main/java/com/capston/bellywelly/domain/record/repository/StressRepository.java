48d4e306eff780421c96edf45f82ebc69f1ede10
yeji <yeji.lee.kim@gmail.com>
2024-04-03 12:37:11 +0900

    feat: 이번주 스트레스 지수를 구하는 서비스 로직 추가

--- src/main/java/com/capston/bellywelly/domain/record/repository/StressRepository.java
+++ src/main/java/com/capston/bellywelly/domain/record/repository/StressRepository.java
@@ -1,8 +1,14 @@
 package com.capston.bellywelly.domain.record.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.capston.bellywelly.domain.member.entity.Member;
 import com.capston.bellywelly.domain.record.entity.Stress;
 
 public interface StressRepository extends JpaRepository<Stress, Long> {
+
+	List<Stress> findAllByMemberAndCreatedDateBetween(Member member, LocalDateTime time1, LocalDateTime time2);
 }
