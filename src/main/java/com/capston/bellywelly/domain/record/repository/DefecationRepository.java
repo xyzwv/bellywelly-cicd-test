d14e639d15c0b419214f91ca88005f4a643665e6
yeji <yeji.lee.kim@gmail.com>
2024-04-03 12:38:01 +0900

    feat: 오늘 배변 현황을 구하는 서비스 로직 추가

--- src/main/java/com/capston/bellywelly/domain/record/repository/DefecationRepository.java
+++ src/main/java/com/capston/bellywelly/domain/record/repository/DefecationRepository.java
@@ -1,8 +1,14 @@
 package com.capston.bellywelly.domain.record.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.capston.bellywelly.domain.member.entity.Member;
 import com.capston.bellywelly.domain.record.entity.Defecation;
 
 public interface DefecationRepository extends JpaRepository<Defecation, Long> {
+
+	List<Defecation> findAllByMemberAndCreatedDateBetween(Member member, LocalDateTime time1, LocalDateTime time2);
 }
