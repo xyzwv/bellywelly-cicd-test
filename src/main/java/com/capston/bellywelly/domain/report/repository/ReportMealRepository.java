0e99fef01abce568d3b82d7d0e356df28e83f577
yeji <yeji.lee.kim@gmail.com>
2024-04-07 23:42:37 +0900

    feat: 식단 레포트 조회를 위한 리포지토리 추가

--- src/main/java/com/capston/bellywelly/domain/report/repository/ReportMealRepository.java
+++ src/main/java/com/capston/bellywelly/domain/report/repository/ReportMealRepository.java
@@ -1,8 +1,13 @@
 package com.capston.bellywelly.domain.report.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.capston.bellywelly.domain.report.entity.Report;
 import com.capston.bellywelly.domain.report.entity.ReportMeal;
 
 public interface ReportMealRepository extends JpaRepository<ReportMeal, Long> {
+
+	List<ReportMeal> findAllByReport(Report report);
 }
--- src/main/java/com/capston/bellywelly/domain/report/repository/ReportRepository.java
+++ src/main/java/com/capston/bellywelly/domain/report/repository/ReportRepository.java
@@ -1,8 +1,13 @@
 package com.capston.bellywelly.domain.report.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.capston.bellywelly.domain.member.entity.Member;
 import com.capston.bellywelly.domain.report.entity.Report;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
+
+	Optional<Report> findByMemberAndYearAndMonthAndWeek(Member member, Integer year, Integer month, Integer week);
 }
