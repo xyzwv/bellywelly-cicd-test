e82e0f7f62c1b5f58e7c27573ebccc82d2ed1764
yeji <yeji.lee.kim@gmail.com>
2024-04-07 23:43:11 +0900

    feat: 식단 레포트 조회를 위한 서비스 및 컨트롤러 메서드 추가

--- src/main/java/com/capston/bellywelly/domain/report/controller/ReportController.java
+++ src/main/java/com/capston/bellywelly/domain/report/controller/ReportController.java
@@ -1,11 +1,14 @@
 package com.capston.bellywelly.domain.report.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.capston.bellywelly.domain.report.dto.DietReportResponseDto;
 import com.capston.bellywelly.domain.report.service.ReportService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -23,5 +26,12 @@ public class ReportController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createReport() {
 		reportService.createReport();
+	}
+
+	@GetMapping("/diet")
+	@ResponseStatus(HttpStatus.OK)
+	public DietReportResponseDto findDietReportByWeek(@RequestParam Integer year, @RequestParam Integer month,
+		@RequestParam Integer week) {
+		return reportService.findDietReport(year, month, week);
 	}
 }
--- src/main/java/com/capston/bellywelly/domain/report/service/ReportService.java
+++ src/main/java/com/capston/bellywelly/domain/report/service/ReportService.java
@@ -9,10 +9,16 @@ import java.util.List;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.capston.bellywelly.domain.member.entity.Member;
+import com.capston.bellywelly.domain.report.dto.DietReportResponseDto;
+import com.capston.bellywelly.domain.report.dto.MealListDto;
+import com.capston.bellywelly.domain.report.entity.Report;
+import com.capston.bellywelly.domain.report.entity.ReportMeal;
 import com.capston.bellywelly.domain.report.repository.ReportDefecationStressRepository;
 import com.capston.bellywelly.domain.report.repository.ReportMealRepository;
 import com.capston.bellywelly.domain.report.repository.ReportRepository;
@@ -51,6 +57,32 @@ public class ReportService {
 		// gpt에 일주일간 식사/배변/스트레스 데이터 보내서 총평, best5/worst5 음식 리스트와 설명, 배변 분석, 스트레스 분석 받기
 
 		// Report, ReportMeal, ReportDefecationStress 생성
+	}
+
+	public DietReportResponseDto findDietReport(Integer year, Integer month, Integer week) {
+		Member member = getCurrentUser();
+		Report report = reportRepository.findByMemberAndYearAndMonthAndWeek(member, year, month, week)
+			.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "해당 레포트가 존재하지 않습니다."));
+		List<ReportMeal> reportMealList = reportMealRepository.findAllByReport(report);
+
+		List<MealListDto> bestMealList = reportMealList.stream()
+			.filter(ReportMeal::getIsBest)
+			.map(this::convertToMealListDto).toList();
+
+		List<MealListDto> worstMealList = reportMealList.stream()
+			.filter(reportMeal -> !reportMeal.getIsBest())
+			.map(this::convertToMealListDto).toList();
+
+		return DietReportResponseDto.builder()
+			.year(report.getYear()).month(report.getMonth()).week(report.getWeek())
+			.feedback(report.getFeedback()).best(bestMealList).worst(worstMealList).build();
+	}
+
+	public MealListDto convertToMealListDto(ReportMeal reportMeal) {
+		return MealListDto.builder()
+			.mealName(reportMeal.getMeal().getMealName())
+			.description(reportMeal.getDescription())
+			.build();
 	}
 
 	public Integer getWeekNumber(LocalDate date) {  // date로부터 주차 수를 구하는 메서드
