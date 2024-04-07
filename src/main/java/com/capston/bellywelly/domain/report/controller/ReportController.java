f445da638abc10f212dbcc4fb16cfec8e45ba989
yeji <yeji.lee.kim@gmail.com>
2024-04-07 22:00:57 +0900

    feat: 레포트 생성 기능 기틀 추가

--- src/main/java/com/capston/bellywelly/domain/report/controller/ReportController.java
+++ src/main/java/com/capston/bellywelly/domain/report/controller/ReportController.java
@@ -1,7 +1,12 @@
 package com.capston.bellywelly.domain.report.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.capston.bellywelly.domain.report.service.ReportService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -11,5 +16,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/reports")
 public class ReportController {
-	
+
+	private final ReportService reportService;
+
+	@PostMapping()
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createReport() {
+		reportService.createReport();
+	}
 }
--- src/main/java/com/capston/bellywelly/domain/report/service/ReportService.java
+++ src/main/java/com/capston/bellywelly/domain/report/service/ReportService.java
@@ -1,7 +1,25 @@
 package com.capston.bellywelly.domain.report.service;
 
+import static com.capston.bellywelly.global.SecurityUtil.*;
+
+import java.time.LocalDate;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.capston.bellywelly.domain.member.entity.Member;
+import com.capston.bellywelly.domain.report.repository.ReportDefecationStressRepository;
+import com.capston.bellywelly.domain.report.repository.ReportMealRepository;
+import com.capston.bellywelly.domain.report.repository.ReportRepository;
+import com.capston.bellywelly.global.feign.client.ChatgptReportClient;
+import com.capston.bellywelly.global.feign.dto.chatgpt.ChatgptReportRequestDto;
+import com.capston.bellywelly.global.feign.dto.chatgpt.ChatgptReportResponseDto;
+import com.capston.bellywelly.global.feign.dto.chatgpt.MessageDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +27,48 @@ import lombok.RequiredArgsConstructor;
 @Transactional
 @RequiredArgsConstructor
 public class ReportService {
+
+	private final ReportRepository reportRepository;
+	private final ReportMealRepository reportMealRepository;
+	private final ReportDefecationStressRepository reportDefecationStressRepository;
+	private final ChatgptReportClient chatgptReportClient;
+
+	@Value("${openai.model}")
+	private String model;
+
+	@Value("${openai.secret-key}")
+	private String openAiKey;
+
+	public void createReport() {  // 일요일 시작 시간에 주간 레포트 자동으로 생성
+
+		// 생성할 레포트 주인, 년, 월, 주차 정보
+		Member member = getCurrentUser();
+		LocalDate yesterday = LocalDate.now().minusDays(1);
+		Integer year = yesterday.getYear();
+		Integer month = yesterday.getMonthValue();
+		Integer week = getWeekNumber(yesterday);
+
+		// gpt에 일주일간 식사/배변/스트레스 데이터 보내서 총평, best5/worst5 음식 리스트와 설명, 배변 분석, 스트레스 분석 받기
+
+		// Report, ReportMeal, ReportDefecationStress 생성
+	}
+
+	public Integer getWeekNumber(LocalDate date) {  // date로부터 주차 수를 구하는 메서드
+		WeekFields weekFields = WeekFields.of(Locale.getDefault());
+		TemporalField weekOfMonth = weekFields.weekOfMonth();
+		return date.get(weekOfMonth);
+	}
+
+	public ChatgptReportResponseDto getChatCompletion() {  // gpt 채팅 결과 받아오는 메서드
+		return chatgptReportClient.getComment(
+			"Bearer " + openAiKey,
+			ChatgptReportRequestDto.builder()
+				.model(model)
+				.messages(List.of(MessageDto.builder()
+					.role("user")
+					.content("(임시) 일주일 식단, 배변, 스트레스 기록 데이터")
+					.build()
+				))
+				.build());
+	}
 }
