3129b46522ed58640605898c34d331a3f734ee2b
yeji <yeji.lee.kim@gmail.com>
2024-04-07 23:55:13 +0900

    feat: 베변/스트레스 레포트 조회를 위한 리포지토리 추가

--- src/main/java/com/capston/bellywelly/domain/report/repository/ReportDefecationStressRepository.java
+++ src/main/java/com/capston/bellywelly/domain/report/repository/ReportDefecationStressRepository.java
@@ -1,8 +1,13 @@
 package com.capston.bellywelly.domain.report.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.capston.bellywelly.domain.report.entity.Report;
 import com.capston.bellywelly.domain.report.entity.ReportDefecationStress;
 
 public interface ReportDefecationStressRepository extends JpaRepository<ReportDefecationStress, Long> {
+
+	List<ReportDefecationStress> findAllByReport(Report report);
 }
